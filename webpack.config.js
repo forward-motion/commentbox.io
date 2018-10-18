ec55744fd7c1460d7434a315cf9537fc95108a59
shaunpersad <shaunpersad@gmail.com>
2018-10-18 00:24:25 -0400

    fixed umd issue

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'commentBox.min.js',
         library: 'commentBox',
-        libraryTarget: 'umd'
+        libraryTarget: 'umd',
+        globalObject: "typeof self !== 'undefined' ? self : this"
     }
 };
